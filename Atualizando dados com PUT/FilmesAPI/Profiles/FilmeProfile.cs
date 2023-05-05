4fe001fa541bb5a2f823e43b09f1206ee9142cac
Rafael <rafaeldealmeida.192001@gmail.com>
2023-05-05 11:16:15 -0300

    Inclui atualizacao com PUT

---
+++ Atualizando dados com PUT/FilmesAPI/Profiles/FilmeProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using FilmesApi.Models;
+using FilmesAPI.Data.DTOs;
+
+namespace FilmesAPI.Profiles;
+
+public class FilmeProfile : Profile
+{
+
+    public FilmeProfile() {
+        CreateMap<CreateFilmeDto, Filme>();
+        CreateMap<UpdateFilmeDto, Filme>();
+    }
+}
